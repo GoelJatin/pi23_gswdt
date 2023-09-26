d99f5a94842f25742a71a26cee8a75b8fbd33368
Jatin Goel <jatin.goel@thesummitgrp.com>
2023-09-27 01:55:18 +0530

    Bank landing page

--- pi23_gswdt/bank/views.py
+++ pi23_gswdt/bank/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Welcome to MY bank")
+    return render(request, "bank/index.html")
